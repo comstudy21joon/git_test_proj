a189b61ee7dc10fc84edee95f729a07e48f0ac6d
comstudy21 <76138542+comstudy21joon@users.noreply.github.com>
2021-10-15 18:07:23 +0900

    Update app.js

--- app.js
+++ app.js
@@ -19,6 +19,10 @@ app.route("/gallery").get((req, res) => {
   res.end("<h1>Gallery page ... </h1>");
 });
 
+app.route("/photo").get((req, res) => {
+  res.end("<h1>Photo page ... </h1>");
+});
+
 app.use("/", route);
 const server = http.createServer(app);
 server.listen(3000, () => {
