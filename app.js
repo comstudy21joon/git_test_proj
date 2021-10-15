c8779809a1da1278408f0880426702d9fa53d73f
comstudy21 <comstudy21@naver.com>
2021-10-15 17:44:59 +0900

    edit app js file

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.route("/").get((req, res) => {
   res.end("<h1>Hello world!</h1>");
 });
 
+app.route("/home").get((req, res) => {
+  res.end("<h1>Home page ... </h1>");
+});
+
 app.use("/", route);
 const server = http.createServer(app);
 server.listen(3000, () => {
